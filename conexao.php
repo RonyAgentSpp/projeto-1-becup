31a74f8d64c8ecfd799ac1e68f07227f74fc77e7
RONALDO LIMA/RONY INSIDE CODE <72349305+RonyAgentSpp@users.noreply.github.com>
2022-10-30 18:27:45 -0300

    # Sistesma Escolar Completo.
    #SISTEMA ESCOLAR COMPLETO COM DASH BOARD DE GESTÃO ESCOLAR, UNIVERSIDADE PORTUGUESA, CADASTRO DE ALUNOS, CONTROLE DE PAGAMENTO,
    LOGIN E SENHA COM VALIDAÇÃO
     Login:  eve.holt@reqres.in
     senha:   cityslicka  
    DIRECT BY: Rony Inside Code -> Ronaldo Lima Dos Santos 
    Contact: 5521970079469
    Front-End Develope

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$hotname ="loccalhost";
+$bancodedados = "Alunos";
+$usuario = "root";
+$senha = "";($hostname, $usuario, @senha,$bancodedados);
+if ($mysqli->connect_errno) { 
+    echo "falha ao conectar: (" .$mysqli->connect_errno. ") ". $mysqli->connect_errno;
+$mysqli = new mysqli
+
+} else
+   echo "Conectado!"
